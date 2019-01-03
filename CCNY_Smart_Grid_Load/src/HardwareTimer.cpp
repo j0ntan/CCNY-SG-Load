7f0e702da5f579b04d0e5a90c651ac4ed2f899f8
j0ntan <jgamboa0209@gmail.com>
2019-01-02 22:03:16 -0500

    Add HardwareTimer class & Timer mock

---
+++ CCNY_Smart_Grid_Load/src/HardwareTimer.cpp
@@ -0,0 +1,8 @@
+#include "../include/HardwareTimer.h"
+#include "Arduino.h"
+
+void HardwareTimer::delay(Timer::milliseconds ms) const { ::delay(ms); }
+
+void HardwareTimer::delayMicroseconds(Timer::microseconds us) const {
+  ::delayMicroseconds(us);
+}
