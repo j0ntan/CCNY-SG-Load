a1d60e1ade1597fdb6a7ef70476fbc9a3aee55c9
j0ntan <jgamboa0209@gmail.com>
2019-01-23 05:04:30 -0500

    Add valid format checker for profile line

--- CCNY_Smart_Grid_Load/src/LoadProfile.cpp
+++ CCNY_Smart_Grid_Load/src/LoadProfile.cpp
@@ -1,5 +1,7 @@
 #include "../include/LoadProfile.h"
+#include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #include "../include/InputSequence.h"
 
 namespace {
@@ -17,6 +19,33 @@ void fillBuffer(char* buffer, uint8_t buf_size, File& file) {
 
 bool lineIsComment(char* buffer) {
   return strlen(buffer) >= 2 && buffer[0] == '/' && buffer[1] == '/';
+}
+
+uint8_t countNumSpacePairs(char* buffer, uint8_t buf_length) {
+  uint8_t pairs_counted = 0;
+  for (uint8_t i = 1; i < buf_length; ++i) {
+    if (isblank(buffer[i]) && isdigit(buffer[i - 1])) {
+      ++pairs_counted;
+    }
+  }
+  return pairs_counted;
+}
+
+bool onlyNumsAndSpaces(char* buffer, uint8_t buf_length) {
+  bool valid_char = true;
+  for (uint8_t i = 0; i < buf_length && valid_char; ++i)
+    valid_char = isdigit(buffer[i]) || isblank(buffer[i]);
+  return valid_char;
+}
+
+bool validFormat(char* buffer) {
+  // minimun requirements is 4 numbers & 3 spaces in-between
+
+  const uint8_t MINIMUM_LENGTH = 7;
+  const uint8_t LENGTH = strlen(buffer);
+  const bool ENDS_WITH_NUM = isdigit(buffer[LENGTH - 1]);
+  return LENGTH >= MINIMUM_LENGTH && onlyNumsAndSpaces(buffer, LENGTH) &&
+         (countNumSpacePairs(buffer, LENGTH) == 3 && ENDS_WITH_NUM);
 }
 
 void addNumbersToSequence(char*& num_begins, char*& num_ends,
