c41a07ca9d25e3b93d25b636f8ecf3b1502b8fcc
j0ntan <jgamboa0209@gmail.com>
2019-02-16 09:28:27 -0500

    Validate sequence length

--- CCNY_Smart_Grid_Load/src/RelayStateGenerator.cpp
+++ CCNY_Smart_Grid_Load/src/RelayStateGenerator.cpp
@@ -1,6 +1,10 @@
 #include "../include/InputSequence.h"
 #include "../include/RelayState.h"
 
-bool isValidSequence(const InputSequence& input) {}
+namespace {
+bool isNotEmpty(const InputSequence& input) { return input.length() != 0; }
+}  // namespace
+
+bool isValidSequence(const InputSequence& input) { return isNotEmpty(input); }
 
 RelayState generateRelayState(const InputSequence& input) {}
--- tests/RelayStateGenerator_tests.cpp
+++ tests/RelayStateGenerator_tests.cpp
@@ -1,2 +1,12 @@
 #include <gtest/gtest.h>
+#include <InputSequence.h>
+#include "InputSequence_utils.h"
 #include <RelayStateGenerator.h>
+
+TEST(ValidateSequence, emptySequenceIsNotValid) {
+  ASSERT_FALSE(isValidSequence(InputSequence{}));
+}
+
+TEST(ValidateSequence, simpleSequenceIsValid) {
+  ASSERT_TRUE(isValidSequence(initialized_Sequence("A1")));
+}
