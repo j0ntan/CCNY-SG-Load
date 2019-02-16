b118537d37193cf1876e870ba6639501acc73d00
j0ntan <jgamboa0209@gmail.com>
2019-02-16 09:43:14 -0500

    Validate sequence chars

--- CCNY_Smart_Grid_Load/src/RelayStateGenerator.cpp
+++ CCNY_Smart_Grid_Load/src/RelayStateGenerator.cpp
@@ -1,10 +1,20 @@
+#include "../src/ValidTokens.h"
 #include "../include/InputSequence.h"
 #include "../include/RelayState.h"
 
 namespace {
 bool isNotEmpty(const InputSequence& input) { return input.length() != 0; }
+
+bool containsOnlyValidChars(const InputSequence& input) {
+  const uint8_t LENGTH = input.length();
+  for (uint8_t i = 0; i < LENGTH; ++i)
+    if (charToToken(input[i]) == Token::INVALID) return false;
+  return true;
+}
 }  // namespace
 
-bool isValidSequence(const InputSequence& input) { return isNotEmpty(input); }
+bool isValidSequence(const InputSequence& input) {
+  return isNotEmpty(input) && containsOnlyValidChars(input);
+}
 
 RelayState generateRelayState(const InputSequence& input) {}
--- tests/RelayStateGenerator_tests.cpp
+++ tests/RelayStateGenerator_tests.cpp
@@ -10,3 +10,26 @@ TEST(ValidateSequence, emptySequenceIsNotValid) {
 TEST(ValidateSequence, simpleSequenceIsValid) {
   ASSERT_TRUE(isValidSequence(initialized_Sequence("A1")));
 }
+
+TEST(ValidateSequence, lowercaseSequenceIsValid) {
+  ASSERT_TRUE(isValidSequence(initialized_Sequence("abc1")));
+}
+
+TEST(ValidateSequence, uppercaseSequenceIsValid) {
+  ASSERT_TRUE(isValidSequence(initialized_Sequence("ABC1")));
+}
+
+TEST(ValidateSequence, mixedcaseSequenceIsValid) {
+  ASSERT_TRUE(isValidSequence(initialized_Sequence("aBc1")));
+}
+
+TEST(ValidateSequence, invalidLetterSequenceIsNotValid) {
+  ASSERT_FALSE(isValidSequence(initialized_Sequence("e1")));
+  ASSERT_FALSE(isValidSequence(initialized_Sequence("Z1")));
+}
+
+TEST(ValidateSequence, invalidSymbolSequenceIsNotValid) {
+  ASSERT_FALSE(isValidSequence(initialized_Sequence("a.1")));
+  ASSERT_FALSE(isValidSequence(initialized_Sequence("a1!")));
+  ASSERT_FALSE(isValidSequence(initialized_Sequence("_a1")));
+}
