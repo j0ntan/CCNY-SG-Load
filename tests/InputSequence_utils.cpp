e24e831d0ddb3b7aa0364c0f6d38565b3345a9ed
j0ntan <jgamboa0209@gmail.com>
2019-02-20 04:31:50 -0500

    Move InputSequence util functions to source file

---
+++ tests/InputSequence_utils.cpp
@@ -0,0 +1,20 @@
+#include <InputSequence.h>
+
+bool operator==(const InputSequence& lhs, const InputSequence& rhs) {
+    bool retval = lhs.length() == rhs.length();
+    if (retval) {
+        const uint8_t len = lhs.length();
+        for (uint8_t i = 0; i < len && retval; ++i) retval = lhs[i] == rhs[i];
+    }
+    return retval;
+}
+
+InputSequence initialized_Sequence(const char* str) {
+    InputSequence retval;
+    retval.addInput(str);
+    return retval;
+}
+
+bool operator==(const InputSequence& lhs, const char* rhs) {
+    return lhs == initialized_Sequence(rhs);
+}
--- tests/InputSequence_utils.h
+++ tests/InputSequence_utils.h
@@ -1,25 +1,12 @@
 #ifndef INPUTSEQUENCE_UTILS_H
 #define INPUTSEQUENCE_UTILS_H
 
-#include <InputSequence.h>
+class InputSequence;
 
-bool operator==(const InputSequence& lhs, const InputSequence& rhs) {
-  bool retval = lhs.length() == rhs.length();
-  if (retval) {
-    const uint8_t len = lhs.length();
-    for (uint8_t i = 0; i < len && retval; ++i) retval = lhs[i] == rhs[i];
-  }
-  return retval;
-}
+bool operator==(const InputSequence& lhs, const InputSequence& rhs);
 
-InputSequence initialized_Sequence(const char* str) {
-  InputSequence retval;
-  retval.addInput(str);
-  return retval;
-}
+InputSequence initialized_Sequence(const char* str);
 
-bool operator==(const InputSequence& lhs, const char* rhs) {
-  return lhs == initialized_Sequence(rhs);
-}
+bool operator==(const InputSequence& lhs, const char* rhs);
 
 #endif  // INPUTSEQUENCE_UTILS_H
